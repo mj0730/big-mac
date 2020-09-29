f3c1e1e81c674a17ec5b2034e16d1e2bd4adf9f8
Michael Johnson <mrj0730@icloud.com>
2020-09-29 01:24:07 -0500

    setup server; get country data from api, currently hard coded with ip address

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const https = require('https');
+
+const app = express();
+const port = 3001;
+
+app.use(express.static('build'));
+
+app.get('/', (req, res) => {
+  const ip = req.ip;
+  res.status(200).send(`your ip is ${ip}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Top from './components/Top'
+import Top from './components/Top';
 import Middle from './components/Middle';
 import Bottom from './components/Bottom';
 
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [country, setCountry] = useState('');
+
+  useEffect(() => {
+    //
+  }, []);
+
   return (
-    <div className="App">
+    <div className='App'>
       <Top />
       <Middle />
       <Bottom />
