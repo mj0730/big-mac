fd8088c66facc73e5e285db492a4bf809de7ff2f
Michael Johnson <mrj0730@icloud.com>
2020-09-30 18:30:30 -0500

    setup unit tests

---
+++ src/App.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import App from './App';
+import Top from './components/Top';
+import Middle from './components/Middle';
+import Bottom from './components/Bottom';
+
+describe('Component rendering', () => {
+  it('renders App without crashing', () => {
+    shallow(<App />);
+  });
+  it('App contains the required sections', () => {
+    const wrapper = mount(<App />);
+
+    expect(wrapper.containsAllMatchingElements([<Top />, <Middle />, <Bottom />])).toEqual(true);
+  });
+  /* other tests */
+});
