b994e6f5613f780abf8d7c526282146f1cd136f1
Michael Johnson <mrj0730@icloud.com>
2020-10-01 11:36:03 -0500

    add icon; placeholders for exchange rate info

--- src/components/Middle.jsx
+++ src/components/Middle.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Middle = ({ amount }) => {
+  const exchangeRate = 2; //usercurrency / dollar
+  const ppp = amount / exchangeRate;
+
   return (
     <section id='middle'>
+      <h3>Dollar Purchasing Parity (PPP)</h3>
       <p>You can buy ## of Big Macs in your country.</p>
-      <p>Yout Dollar Purchasing Parity (PPP) is #</p>
+      <p>Your Dollar Purchasing Parity is {ppp}</p>
     </section>
   );
 };
